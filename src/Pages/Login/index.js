fdeb4451241ea61275e9fd0f66b70800f0fa8dd2
FIREPC\FIRE <firebenderyt@gmail.com>
2023-04-21 20:05:13 +0300

    Added redirect from login page to profile for authentificated user

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "@contexts";
 import { SignInPopup } from "@components/smart";
 
 const Login = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  if (isAuthenticated) {
+    return <Navigate to="/profile" />;
+  }
+
   return <SignInPopup />;
 };
 
