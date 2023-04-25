3d83e9f3c9618dda3f55eff6841affc254957b03
kirill <kirillulianow@gmail.com>
2023-04-25 14:58:01 +0300

    Added SignUp Page component

---
+++ src/Pages/SignUpPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignUpForm from '../../Components/smart/SignUpForm';
+import "./SignUpPage.scss";
+
+const SignUpPage = () => {
+    return (
+        <SignUpForm/>
+    );
+};
+
+export default SignUpPage;
