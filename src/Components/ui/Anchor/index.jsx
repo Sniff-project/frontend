da7a4a6bd082d83d2511dbe421ae849d8276b7c3
FIREPC\FIRE <firebenderyt@gmail.com>
2023-04-27 17:37:35 +0300

    comment removed + unused package removed

--- src/Components/ui/Anchor/index.jsx
+++ src/Components/ui/Anchor/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate, useLocation, Link } from "react-router-dom";
-// import { HashLink as Link } from "react-router-hash-link";
 
 export default function Anchor({ href, text, className, color = "white" }) {
   const { pathname } = useLocation();
