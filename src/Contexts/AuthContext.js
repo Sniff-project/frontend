6a2d4cfdbd1c53cf431a87883afc761bb41a4006
FIREPC\FIRE <firebenderyt@gmail.com>
2023-04-29 22:27:06 +0300

    auth context modified

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,17 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const isAuth = sessionStorage.getItem("jwtToken") ? true : false;
+  const token = sessionStorage.getItem("jwtToken");
+  const isAuth = token ? true : false;
 
   const [isAuthenticated, setIsAuthenticated] = useState(isAuth);
+  const [user, setUser] = useState(null);
   const history = useNavigate();
 
-  const login = (token) => {
+  const login = (jwtToken) => {
     setIsAuthenticated(true);
-    sessionStorage.setItem("jwtToken", token);
+    sessionStorage.setItem("jwtToken", jwtToken);
     history("/profile");
   };
 
@@ -20,8 +23,28 @@ export const AuthProvider = ({ children }) => {
     sessionStorage.removeItem("jwtToken");
   };
 
+  useEffect(() => {
+    if (isAuthenticated) {
+      try {
+        const decoded = jwt_decode(token);
+        const currentTime = Date.now() / 1000;
+        setUser(decoded.name);
+
+        // Check if token has expired
+        if (decoded.exp < currentTime) {
+          logout();
+        }
+      } catch (error) {
+        console.error("Invalid token");
+        logout();
+      }
+    }
+  }, [isAuthenticated, token]);
+
   const value = {
+    token,
     isAuthenticated,
+    user,
     login,
     logout,
   };
