4e94c4b9e5588d1d05c75fb7b6c9db14bbfbed7e
Yana <yanochkalaz0560@gmail.com>
2023-05-09 19:03:30 +0300

    Full adaptive navbar and homepage

--- src/Containers/Homepage/AboutUs/index.jsx
+++ src/Containers/Homepage/AboutUs/index.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Button1 } from "@components/ui";
 import "./style.scss";
 
 export default function AboutUs() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
+
+  const handleResize = (event) => {
+    setScreenWidth(event.target.innerWidth);
+  };
+
   return (
     <section id="aboutUs">
       <div className="about-bg">
         <div className="about-block">
-          <h1 className="about-block__title">SNIFF допоможе знайти вашу тваринку</h1>
-          <p className="about-block__text">Цей веб-ресурс був розроблений для пошуку загублених або залишених тварин.</p>
-          <NavLink to="/about">
-            <Button1 className="btn-blue">Дізнатись більше</Button1>
-          </NavLink>
+          <h1 className="about-block__title">
+            SNIFF допоможе знайти вашу тваринку
+          </h1>
+
+          <div className={screenWidth > 550 ? "" : "about-block__down"}>
+            <p className="about-block__text">
+              Цей веб-ресурс був розроблений для пошуку загублених або залишених
+              тварин.
+            </p>
+            <NavLink to="/about">
+              <Button1 className="btn-blue">Дізнатись більше</Button1>
+            </NavLink>
+          </div>
         </div>
       </div>
     </section>
