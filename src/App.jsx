ff5b65f5211b0e581fd751ef3cc3fbcf0fbae45f
FIREPC\FIRE <firebenderyt@gmail.com>
2023-05-11 17:46:48 +0300

    restored Suspend and lazy for loading App

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useEffect, useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "@contexts";
 import { Header } from "@containers/Navbar";
 import Footer from "@containers/Footer";
-import { Spinner } from "@components/simple";
 import { lightTheme } from "@core/Theme";
 import AppRoutes from "./Routes";
 import "./Styles/App.scss";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <ThemeProvider theme={lightTheme}>
-          <Router>
-            <AuthProvider>
-              <Header />
-              <AppRoutes />
-              <Footer />
-            </AuthProvider>
-          </Router>
-        </ThemeProvider>
-      )}
-    </>
+    <ThemeProvider theme={lightTheme}>
+      <Router>
+        <AuthProvider>
+          <Header />
+          <AppRoutes />
+          <Footer />
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import App from "./App";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@core/Store";
-
+import { Spinner } from "@components/simple";
 import "./Styles/index.scss";
+
+const App = lazy(() => import("./App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
+    <Suspense fallback={<Spinner />}>
       <App />
+    </Suspense>
   </Provider>
 );
