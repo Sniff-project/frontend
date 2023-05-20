44393cd20c987807dcf520155adcc09a5c5cfe27
Yana <yanochkalaz0560@gmail.com>
2023-05-20 11:08:04 +0300

    Fixed a bug with navbar

--- src/Pages/PetsGallery/index.jsx
+++ src/Pages/PetsGallery/index.jsx
@@ -4,14 +4,21 @@ import { AuthContext } from "@contexts";
 import { petsGallery } from "@core/Services/pets";
 
 export default function PetsGallery() {
+  const maxCardsOnPage = 12;
   const { user, token } = useContext(AuthContext);
   const dispatch = useDispatch();
   const gallery_Array = useSelector((state) => state.gallery);
+  const test_Array = new Array(50).fill(1).map((elem, i) => elem = ++i); // from 1 to 50 numbers
 
 
   useEffect(() => {
     dispatch(petsGallery(token));
   }, [dispatch, user, token]);
 
-  return <div>Gallery</div>;
+  return (
+    <div>
+      <div>Gallery</div>
+
+    </div>
+  );
 }
