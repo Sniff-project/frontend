7f4ca3f5e1ad2e0a51435cf875f05e441290763c
Yana <yanochkalaz0560@gmail.com>
2023-05-25 00:02:37 +0300

    LocalStore

--- src/Core/API/pets/index.js
+++ src/Core/API/pets/index.js
@@ -1,4 +1,4 @@
 export { createPetProfile, editPetProfile, getPetProfile } from "./profile";
 export { getGallery } from "./getPetsGallery";
-export {filtration } from "./filtration/filtration";
+export { filtration } from "./filtration";
 export { uploadPhotosPetProfile } from "./uploadPhotos";
