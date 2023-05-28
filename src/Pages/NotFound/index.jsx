07329b1580acbea62200eb9f6b807eef5961e167
kirill <kirillulianow@gmail.com>
2023-05-28 14:13:49 +0300

    404 page layout created + added additional fw for Montserrat + added pattern for 404

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./style.scss";
 
 const NotFound = () => {
+  const pictureRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      let x = -e.clientX / 5;
+      let y = -e.clientY / 5;
+      pictureRef.current.style.backgroundPositionX = x + 'px';
+      pictureRef.current.style.backgroundPositionY = y + 'px';
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Page not found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
+    <section className="NotFound" ref={pictureRef}>
+      <div className="NotFound__container">
+        <h2 className="NotFound__title">404</h2>
+        <h4 className="NotFound__subtitle">Opps! Page not found</h4>
+        <p className="NotFound__text">The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved.</p>
+        <a href="#" className="NotFound__link">Back To Home</a>
+      </div>
+    </section>
   );
 };
 
