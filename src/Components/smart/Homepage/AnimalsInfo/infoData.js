f8ccc5df7ba3c335068805ea6fa06190a784f763
FIREPC\FIRE <firebenderyt@gmail.com>
2023-06-01 16:04:35 +0300

    legendary bug fixed

--- src/Components/smart/Homepage/AnimalsInfo/infoData.js
+++ src/Components/smart/Homepage/AnimalsInfo/infoData.js
@@ -1,27 +1,27 @@
-import vaccination from '../../../../Assets/Icons/AnimalsInformation/vaccination.svg';
-import passport from '../../../../Assets/Icons/AnimalsInformation/passport.svg';
-import dog1 from '../../../../Assets/Icons/AnimalsInformation/dog1.svg';
-import dog2 from '../../../../Assets/Icons/AnimalsInformation/dog2.svg';
+import vaccination from "../../../../Assets/Icons/AnimalsInformation/vaccination.svg";
+import passport from "../../../../Assets/Icons/AnimalsInformation/passport.svg";
+import dog1 from "../../../../Assets/Icons/AnimalsInformation/dog1.svg";
+import dog2 from "../../../../Assets/Icons/AnimalsInformation/dog2.svg";
 
 export const infoArray = [
-    {
-        title: 'Вакцинація',
-        img: vaccination,
-        text: 'Наші тваринки вакциновані та здорові. Усі пройшли нашого ветеренара та мають відповідні щеплення, тож вони гарні та здорові.'
-    },
-    {
-        title: 'Пасторт',
-        img: passport,
-        text: 'Усі мають свою ветеринарну картку де прописані усі щеплення та вони готові подорожувати світом разом з вами.'
-    },
-    {
-        title: 'Щастя',
-        img: dog1,
-        text: 'Тваринки дуже полюбляють коли з ними проводять час та граються.'
-    },
-    {
-        title: 'Вирішив узяти додому хвостатого друга ?',
-        img: dog2,
-        text: 'Чудово, зателефонуй за телефоном що вказан у профілі тваринки. Будь готовий відповісти на декілька запитань, бо різні притулки мають свої '
-    }
-]
+  {
+    title: "Вакцинація",
+    img: vaccination,
+    text: "Наші тваринки вакциновані та здорові. Усі пройшли нашого ветеренара та мають відповідні щеплення, тож вони гарні та здорові.",
+  },
+  {
+    title: "Паспорт",
+    img: passport,
+    text: "Усі мають свою ветеринарну картку де прописані усі щеплення та вони готові подорожувати світом разом з вами.",
+  },
+  {
+    title: "Щастя",
+    img: dog1,
+    text: "Тваринки дуже полюбляють коли з ними проводять час та граються.",
+  },
+  {
+    title: "Вирішив узяти додому хвостатого друга ?",
+    img: dog2,
+    text: "Чудово, зателефонуй за телефоном що вказан у профілі тваринки. Будь готовий відповісти на декілька запитань, бо різні притулки мають свої ",
+  },
+];
